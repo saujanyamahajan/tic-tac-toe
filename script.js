635e8f351ecfe4d3c0ccd531fd04be478b2099de
saujanyamahjan <saujanya.9919@gmail.com>
2022-01-05 21:04:12 +0530

    made ui better

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 console.log("Welcome to Tic Tac Toe")
-
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
 let turn = "X"
 let isgameover = false;
 
-// Func to change turn
+// Function to change the turn
 const changeTurn = ()=>{
     return turn === "X"? "0": "X"
 }
 
-// Func to check for win
+// Function to check for a win
 const checkWin = ()=>{
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
@@ -32,7 +34,8 @@ const checkWin = ()=>{
     })
 }
 
-// Main Logic
+// Game Logic
+// music.play()
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
     let boxtext = element.querySelector('.boxtext');
@@ -49,7 +52,7 @@ Array.from(boxes).forEach(element =>{
     })
 })
 
-// reset button
+// Add onclick listener to reset button
 reset.addEventListener('click', ()=>{
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element => {
